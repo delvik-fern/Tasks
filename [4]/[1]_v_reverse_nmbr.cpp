9c0d13654d1052103640973a2287d4755b7e10d8
Delvik Fern <delvik.fern@yandex.ru>
2016-06-21 23:21:18 +0300

    Additional modification of [1] task

--- [4]/[1]_v_reverse_nmbr.cpp
+++ [4]/[1]_v_reverse_nmbr.cpp
@@ -4,30 +4,47 @@
 
 int main(int argc, char** argv) {
   
-  int* k = new int();
+  int Number;
   printf("Enter a number: ");
-  scanf("%i", k);
-  int i = 1;
+  scanf("%i", &Number);
 
-  
-  int* k_original = new int();
-  *k_original = *k;
+  int number_original = Number;
+  int length = 0;
 
-  while ((*k /= 10) != 0)
-    ++i;
-//      printf("%i\t%i\n", k, i);
- 
-  *k = *k_original;
-  int* arr = new int[i];
-
-  printf("The reverse number is: ");
-
-  for (int j = 0; j < i; ++j) {
-    printf("%i", arr[j] = *k % 10);
-    *k /= 10;
+  while (Number != 0) {
+    Number /= 10;
+    ++length;
   }
 
-  printf("\n");
+  Number = number_original;
+  int reverse_number = 0;
 
-return 0;
+  while (length != 0) {
+    int c = Number % 10;
+
+    for (int j = 1; j != length; ++j) {
+      c *= 10;
+    }
+
+    reverse_number += c;    
+    Number /= 10;
+    --length;
+  } 
+
+  printf("The reverse number is: %i\n", reverse_number);
+
+  return 0;
 }
+
+
+/*
+  while (k != 0) {
+    ++i;
+    printf("k = %i\t", k);
+    printf("k_ost = %i\t", k % 10);
+    printf("i = %i\n", i);
+    k /= 10;
+  }
+  printf("k = %i\n", k);
+*/
+
